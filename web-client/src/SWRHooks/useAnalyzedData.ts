9e12ac29baf576560efb07269c3bca83d9ddb5f6
greedy-hacker <ljhhasang@naver.com>
2022-05-27 16:12:05 +0900

    Fix: listView 문제 수정

--- web-client/src/SWRHooks/useAnalyzedData.ts
+++ web-client/src/SWRHooks/useAnalyzedData.ts
@@ -9,11 +9,11 @@ export class SiteInfo {
   language!: string;
   category!: string;
   reference_url!: string;
-  // @Transform(({value}) => {return JSON.parse(value.replace(/'/g, '"'))})
+  @Transform(({value}) => {return JSON.parse(value.replace(/'/g, '"'))})
   site_tracking_codes!: { [key: string]: string[] };
-  // @Transform(({value}) => {return JSON.parse(value.replace(/'/g, '"'))})
+  @Transform(({value}) => {return JSON.parse(value.replace(/'/g, '"'))})
   personal_information!: { [key: string]: string[] };
-  // @Transform(({value}) => {return JSON.parse(value.replace(/'/g, '"'))})
+  @Transform(({value}) => {return JSON.parse(value.replace(/'/g, '"'))})
   others!: { [key: string]: string[] };
 }
 
